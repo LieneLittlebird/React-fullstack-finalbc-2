a1a27972ed5dbe10b0d7f463c9e9007cb472306c
Liene Putniņa <liene.putnina20@gmail.com>
2020-12-22 15:16:22 +0200

    Fixed ESLint import/export issue

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,17 @@
 module.exports = {
+  settings: {
+    "import/resolver": {
+      alias: {
+        map: [
+          ["babel-polyfill", "babel-polyfill/dist/polyfill.min.js"],
+          ["helper", "./utils/helper"],
+          ["material-ui/DatePicker", "../custom/DatePicker"],
+          ["material-ui", "material-ui-ie10"],
+        ],
+        extensions: [".ts", ".js", ".jsx", ".json"],
+      },
+    },
+  },
   env: {
     browser: true,
     es2021: true,
@@ -14,5 +27,6 @@ module.exports = {
   plugins: ["react"],
   rules: {
     "linebreak-style": ["warn", "windows"],
+    "import/extensions": 0,
   },
 };
