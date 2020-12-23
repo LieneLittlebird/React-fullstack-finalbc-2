063d9277e93ef5a5f02003bb3c551555e01146e0
Liene Putniņa <liene.putnina20@gmail.com>
2020-12-23 17:04:27 +0200

    chat to json placeholder  started

--- src/Components/Discussion/Discussion.jsx
+++ src/Components/Discussion/Discussion.jsx
@@ -1,28 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import "./discussion.css";
 
-const Discussion = () => (
-  <div>
-    <h3>Discussion</h3>
-    <div id="chat-window">
-      <div id="chat-postarea" />
-      <div>
-        <label htmlFor="chat-username">
-          <input type="text" className="chat-data" id="chat-username" />
-          <strong>Username</strong>:
-        </label>
-      </div>
-      <div>
-        <label htmlFor="chat-message">
-          <textarea type="text" id="chat-message" className="chat-data" />
-          <strong>Message</strong>:
-        </label>
-      </div>
-      <button id="submit-button" type="submit">
-        Submit
-      </button>
+const Discussion = () => {
+  const postData = {
+    // 1. Connect username and message field with button
+    // Connect button to saving the data
+  };
+  return (
+    <div>
+      <h3>Discussion</h3>
+      <form>
+        <div id="chat-window">
+          <div id="chat-postarea" />
+          <div>
+            <label htmlFor="chat-username">
+              <input
+                type="text"
+                className="chat-data"
+                id="chat-username"
+                name="username"
+              />
+              <strong>Username</strong>:
+            </label>
+          </div>
+          <div>
+            <label htmlFor="chat-message">
+              <textarea
+                type="text"
+                id="chat-message"
+                className="chat-data"
+                name="message"
+              />
+              <strong>Message</strong>:
+            </label>
+          </div>
+          <button id="submit-button" type="submit" onClick={postData}>
+            Submit
+          </button>
+        </div>
+      </form>
     </div>
-  </div>
-);
+  );
+};
 
 export default Discussion;
